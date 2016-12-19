b3cbf013ebcdf03816bc97324016acbcad5971c5
Giulio Tani <giulio.tani@bandyer.com>
2016-12-19 23:08:22 +0100

    New Folder

---
+++ Database/001_1 - Contiguous_negative_sum_Subarray.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution001 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = s.nextInt();
+        }
+
+        System.out.println(solution(numbers));
+    }
+
+    public static int solution(int[] arr) {
+        int counter = 0;
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            int totalSum = arr[i];
+            if (totalSum < 0) {
+                counter++;
+            }
+
+            for (int j = i + 1; j < n; j++) {
+                totalSum += arr[j];
+
+                if (totalSum < 0) {
+                    counter++;
+                }
+            }
+        }
+
+        return counter;
+    }
+}
+
+// https://www.hackerrank.com/challenges/java-negative-subarray
+// Time O(n^2)
